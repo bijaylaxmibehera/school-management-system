3bbec0a105ba0963600a0775bf5ad7c198eb96a9
Bijaylaxmi Behera <72284560+bijaylaxmibehera@users.noreply.github.com>
2024-02-12 12:27:18 +0530

    minor change

--- src/components/StudentView.jsx
+++ src/components/StudentView.jsx
@@ -1,29 +1,37 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import { StudentList } from "../features/student/StudentList";
 import { Link } from "react-router-dom";
-import { useDispatch,useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchStudents } from "../features/student/studentSlice";
 
-export const StudentView=()=>{
-    const dispatch=useDispatch();
-    const students=useSelector((state)=>state.students.students);
-    const status=useSelector((state)=>state.students.status);
-    const error=useSelector((state)=>state.students.error);
-    
-    useEffect(()=>{
-        if(status==='idle'){
-            dispatch(fetchStudents());
-        }
-    },[status,dispatch])
-    return (
+export const StudentView = () => {
+  const dispatch = useDispatch();
+  const students = useSelector((state) => state.students.students);
+  const status = useSelector((state) => state.students.status);
+  const error = useSelector((state) => state.students.error);
+
+  useEffect(() => {
+    if (status === "idle") {
+      dispatch(fetchStudents());
+    }
+  }, [status, dispatch]);
+  return (
+    <div>
+      <h2>Students view</h2>
+      {status === "loading" ? (
+        <p>Loading...</p>
+      ) : (
         <div>
-          <h1>Student View</h1>
-          {status==='loading' && <p>Loading...</p>}
-          {error && <p>Error:{error}</p>}
-          <StudentList students={students}/>
-          <h3>
-            <Link to={`/students/add`}>Add student</Link>
-          </h3>
+          {status === "error" ? (
+            error
+          ) : students.length === 0 ? (
+            <p>No students found</p>
+          ) : (
+            <StudentList students={students} />
+          )}
         </div>
-    )
-}
+      )}
+      <Link to={`/students/add`}>Add student</Link>
+    </div>
+  );
+};
